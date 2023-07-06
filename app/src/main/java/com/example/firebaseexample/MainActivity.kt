70ac0e6bcaa6f815df9284247f8097b6aade09e4
Eric McKevitt <erictmckevitt@gmail.com>
2023-07-05 21:44:33 -0500

    Added read functionality to observe user's name

--- app/src/main/java/com/example/firebaseexample/MainActivity.kt
+++ app/src/main/java/com/example/firebaseexample/MainActivity.kt
@@ -2,18 +2,25 @@ package com.example.firebaseexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var editText: EditText
     lateinit var buttonSend: Button
+    lateinit var textViewName: TextView
 
     val database: FirebaseDatabase = FirebaseDatabase.getInstance()
     val reference: DatabaseReference = database.reference.child("Users")
+    val reference2: DatabaseReference = database.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,21 @@ class MainActivity : AppCompatActivity() {
 
         editText = findViewById(R.id.editTextName)
         buttonSend = findViewById(R.id.buttonSend)
+        textViewName = findViewById(R.id.textViewName)
+
+        reference2.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val realName: String = snapshot.child("Users").child("name").value as String
+
+                textViewName.text = realName
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
 
         buttonSend.setOnClickListener {
             val userName: String = editText.text.toString()
